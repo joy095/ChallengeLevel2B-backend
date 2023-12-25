f775c7ec0998eddf46e1984803dfbd556d21e212
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-12-25 19:12:24 +0530

    cors update

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/test", (req, res) => {
   res.send("testing");
